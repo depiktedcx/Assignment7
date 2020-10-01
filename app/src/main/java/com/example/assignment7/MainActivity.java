6b74614c59ebbc81c6082597eaf166ce9e5fdd77
xz298 <vince0200612J@gmail.com>
2020-10-02 12:14:02 +1300

    commit <add>-- create game page and start by button

--- app/src/main/java/com/example/assignment7/MainActivity.java
+++ app/src/main/java/com/example/assignment7/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.assignment7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClickStart(View D){
+
+       Intent gameMain = new Intent(this,GameScreanMain.class);
+        startActivity(gameMain);
     }
 }
