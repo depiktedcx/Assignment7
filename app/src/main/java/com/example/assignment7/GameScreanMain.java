ed87fdac063703b480f869c7188686c5f043d0de
xz298 <vince0200612J@gmail.com>
2020-10-02 14:28:30 +1300

    commit <add>-- override Ondraw and canvas

--- app/src/main/java/com/example/assignment7/GameScreanMain.java
+++ app/src/main/java/com/example/assignment7/GameScreanMain.java
@@ -2,11 +2,19 @@ package com.example.assignment7;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.View;
 
 public class GameScreanMain extends AppCompatActivity {
+
+
+    Paint piant2 = new Paint();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +29,33 @@ public class GameScreanMain extends AppCompatActivity {
 
         int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY|View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
         getWindow().getDecorView().setSystemUiVisibility(uiOptions);
+
+
+        GraphicsViewGame graphicsView_1 = new GraphicsViewGame(this);
+        ConstraintLayout constraintLayout = findViewById(R.id.gamepage);
+        constraintLayout.addView(graphicsView_1);
+
+    }
+
+
+    public class GraphicsViewGame extends View{
+
+       // private GestureDetector gestureDetector_1;
+
+
+
+        public GraphicsViewGame(Context context) {
+            super(context);
+            piant2.setColor(getColor(R.color.black_overlay));
+           // gestureDetector_1 = new
+
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            canvas.drawCircle(100, 100, 76, piant2);
+
+        }
     }
 }
