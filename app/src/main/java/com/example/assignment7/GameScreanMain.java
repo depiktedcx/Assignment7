044a9a6f6eae74924a9afdf9c12f975f380656e2
xz298 <vince0200612J@gmail.com>
2020-10-02 13:44:05 +1300

    commit <add>-- use immersive sticky model

--- app/src/main/java/com/example/assignment7/GameScreanMain.java
+++ app/src/main/java/com/example/assignment7/GameScreanMain.java
@@ -1,8 +1,10 @@
 package com.example.assignment7;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class GameScreanMain extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class GameScreanMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screan);
+
+
+
+        ActionBar actionbar1 = getSupportActionBar();
+        actionbar1.hide();
+
+
+        int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY|View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        getWindow().getDecorView().setSystemUiVisibility(uiOptions);
     }
 }
