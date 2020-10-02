f8a2ad41ae4375cb624b4ec582eaa53d7beb8b0e
xz298 <vince0200612J@gmail.com>
2020-10-02 15:00:05 +1300

    commit <add>-- Use flling not test

--- app/src/main/java/com/example/assignment7/GameScreanMain.java
+++ app/src/main/java/com/example/assignment7/GameScreanMain.java
@@ -8,13 +8,17 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class GameScreanMain extends AppCompatActivity {
 
 
     Paint piant2 = new Paint();
+
+    String TAG_Gestrue="Gestrue" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,16 +44,34 @@ public class GameScreanMain extends AppCompatActivity {
 
     public class GraphicsViewGame extends View{
 
-       // private GestureDetector gestureDetector_1;
+       private GestureDetector gestureDetector_1;
 
 
 
         public GraphicsViewGame(Context context) {
             super(context);
             piant2.setColor(getColor(R.color.black_overlay));
-           // gestureDetector_1 = new
+            gestureDetector_1 = new GestureDetector(context,new MyGestureListener());
 
         }
+
+
+        class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+            @Override
+            public boolean onDown(MotionEvent e) {
+                Log.i(TAG_Gestrue, "onDOWN");
+                return true;
+            }
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2,
+                                   float velocityX, float velocityY) {
+                Log.i(TAG_Gestrue, "flling~~~~~~~~~~~~~~~~");
+                return true;
+            }
+        }
+
+
+
 
         @Override
         protected void onDraw(Canvas canvas) {
